47e9f60caf71f00dc80687811d7ce81975393327
Ramon Hoyo <ramonandreshoyodiaz@gmail.com>
2024-06-28 18:40:11 -0400

    update

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -9,7 +9,7 @@ export default function Skills() {
 
       <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
         <Image src="/in_development.jpg" alt="In Development" width={800} height={500} />
-        <h3>Under Development</h3>
+        <h3>Under Development...</h3>
       </div>
     </section>
   )
