8a6399675f3aa020a932a0c24091769297d4060f
Ramon Hoyo <ramonandreshoyodiaz@gmail.com>
2024-07-01 17:45:27 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import HeaderSection from "@/header/HeaderSection";
 import IntroSection from "@/intro/IntroSection";
 import Skills from "@/skills/Skills";
 import { Container } from "@mui/material";
 import Profile from "@/profile/Profile";
+import Projects from "@/projects/Projects";
+import Contact from "@/contact/Contact";
 
 export default function Home() {
   return (
@@ -13,21 +14,11 @@ export default function Home() {
 
       <Container>
         <IntroSection />
-
         <Profile />
-
-        <br />
-
         <Skills />
+        <Projects />
+        <Contact />
       </Container>
-
-      <div className={styles.description}>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <div style={{ flex: 1 }}>
-          </div>
-        </div>
-      </div>
-
     </main>
   );
 }
--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Image from 'next/image'
 import { Grid, Paper, Typography } from '@mui/material'
 
-export default function Skills() {
+export default function Contact() {
   return (
     <Paper sx={{ my: 2, p: 2 }}>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Typography variant='h3'>Skills</Typography>
+          <Typography variant='h3'>Contact</Typography>
         </Grid>
         <Grid item xs={12}>
           <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
