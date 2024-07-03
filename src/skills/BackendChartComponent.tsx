96adf6bce5a167d3078918a71355a148c22bdcc0
Ramon Hoyo <ramonandreshoyodiaz@gmail.com>
2024-07-03 11:19:25 -0400

    upload

--- src/skills/BackendChartComponent.tsx
+++ src/skills/BackendChartComponent.tsx
@@ -1,39 +1,15 @@
 "use client";
 import { green } from '@mui/material/colors';
 import React from 'react';
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer, Tooltip } from 'recharts';
 
 const data = [
-  {
-    subject: 'TypeORM (MySQL, Postgres)',
-    A: 120,
-    fullMark: 150,
-  },
-  {
-    subject: 'Mongoose (MongoDB)',
-    A: 130,
-    fullMark: 150,
-  },
-  {
-    subject: 'Swagger',
-    A: 86,
-    fullMark: 150,
-  },
-  {
-    subject: 'GraphQL',
-    A: 99,
-    fullMark: 150,
-  },
-  {
-    subject: 'Redis',
-    A: 85,
-    fullMark: 150,
-  },
-  {
-    subject: 'BullMQ',
-    A: 120,
-    fullMark: 150,
-  },
+  { subject: 'TypeORM (MySQL, Postgres)', A: 80 },
+  { subject: 'Mongoose (MongoDB)', A: 85 },
+  { subject: 'Swagger', A: 90 },
+  { subject: 'GraphQL', A: 75 },
+  { subject: 'Redis', A: 80 },
+  { subject: 'BullMQ', A: 80 },
 ];
 
 export default function BackendChartComponent() {
@@ -41,8 +17,9 @@ export default function BackendChartComponent() {
     <ResponsiveContainer minHeight={300} width="100%" height="100%">
       <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
         <PolarGrid />
-        <PolarAngleAxis dataKey="subject" />
+        <PolarAngleAxis dataKey="subject" domain={[0, 100]} />
         <PolarRadiusAxis />
+        <Tooltip />
         <Radar name="Backend" dataKey="A" stroke={green[500]} fill={green[200]} fillOpacity={0.6} />
       </RadarChart>
     </ResponsiveContainer>
