081e3c59219639eb57b7e31f6bdc8af500fe79b9
Ramon Hoyo <ramonandreshoyodiaz@gmail.com>
2024-07-16 18:43:07 -0400

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme';
 import CssBaseline from '@mui/material/CssBaseline';
+import GoogleAnalytics from "@/utils/GoogleAnalytics";
 
 export const metadata: Metadata = {
   title: "Ramón Hoyo",
@@ -21,6 +21,7 @@ export default function RootLayout({
       <AppRouterCacheProvider>
         <ThemeProvider theme={theme}>
           <CssBaseline />
+          <GoogleAnalytics />
           <body>
             {children}
           </body>
